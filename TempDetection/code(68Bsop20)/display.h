697c201c277dc94d23b1d4fea1a41609026a6385
Gump <870705273@qq.com>
2019-12-30 16:58:23 +0800

    feat: 简化 LED 宏

--- TempDetection/code(68Bsop20)/display.h
+++ TempDetection/code(68Bsop20)/display.h
@@ -4,24 +4,19 @@
 #define P_led_1 P1_4
 #define P_led_2 P3_6
 #define P_led_com P1_0
+
 // Const: exp: #define D_data 1 ----------------
-#define D_ledOn1 1  //高电位亮
-#define D_ledOff1 0 //低点位灭
-#define D_ledOn2 1  //高电位亮
-#define D_ledOff2 0 //低点位灭
+#define D_ledOn 1  //高电位亮
+#define D_ledOff 0 //低点位灭
 // Globle Var -----------------------------------------
 #ifdef __display_c
 #else
-
 #endif
 
 // Action Macro: exp: #define F_getData() ------
-#define F_ledOn1() P_led_1 = D_ledOn1
-#define F_ledOff1() P_led_1 = D_ledOff1
-#define F_ledNeg1() P_led_1 = ~P_led_1
-#define F_ledOn2() P_led_2 = D_ledOn2
-#define F_ledOff2() P_led_2 = D_ledOff2
-#define F_ledNeg2() P_led_2 = ~P_led_2
+#define F_ledOn(no) P_led_##no = D_ledOn
+#define F_ledOff(no) P_led_##no = D_ledOff
+#define F_ledNeg(no) P_led_##no = ~P_led_##no
 // Function ------------------------------------
 extern void LED_Disable(void);
 #endif
